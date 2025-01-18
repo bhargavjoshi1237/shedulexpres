398ed865798150b72caba388edff96c4b9a18831
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2025-01-18 06:25:55 +0000

    fixed delete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 
 app.use(cors({
   origin: ['http://localhost:3000'],
-  methods: ['GET', 'POST','PUT'],
+  methods: ['GET', 'POST','PUT','DELETE'],
   credentials: true
 }));
 
