1fb9355adc04def1f50a542999a890e01a0fff07
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2025-01-18 04:54:17 +0000

    added cors suppoert

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const { Pool } = require('pg');
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 
 app.use(express.json());
 const pool = new Pool({
