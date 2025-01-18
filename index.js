ac6f77003a4763d2aee986c7304443d29a03537e
Bhargav Joshi <68766403+bhargavjoshi1237@users.noreply.github.com>
2025-01-18 06:26:44 +0000

    fex

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const { Pool } = require('pg');
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000'],f
   methods: ['GET', 'POST','PUT','DELETE'],
   credentials: true
 }));
